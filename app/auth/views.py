aa2b9fb592af57d93137f083ca49b65a279fab5a
juan carlos <juancarlosmz547@gmail.com>
2023-09-04 21:39:59 -0500

    realizando blueprints

---
+++ app/auth/views.py
@@ -0,0 +1,15 @@
+
+from app.forms import LoginForm
+from flask import render_template
+
+from . import auth
+
+
+
+
+@auth.route('/login')
+def login():
+    context={
+        'login_form':LoginForm()
+    }
+    return render_template('login.html', **context)
